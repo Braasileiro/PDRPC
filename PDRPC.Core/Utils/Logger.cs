cc4b217194d5c746c324181a8ba068b57499f381
Lucas Cota <lucasbrunocota@live.com>
2022-07-11 16:33:55 -0300

    More generic logger.

--- PDRPC.Core/Utils/Logger.cs
+++ PDRPC.Core/Utils/Logger.cs
@@ -4,17 +4,17 @@ internal static class Logger
 {
     private static readonly string PREFIX = "[PDRPC]";
 
-    public static void Info(string message)
+    public static void Info(object message)
     {
         Console.WriteLine($"{PREFIX}: {message}");
     }
 
-    public static void Warning(string message)
+    public static void Warning(object message)
     {
         Console.WriteLine($"{PREFIX}[WARN]: {message}");
     }
 
-    public static void Error(string message)
+    public static void Error(object message)
     {
         Console.WriteLine($"{PREFIX}[ERROR]: {message}");
     }
