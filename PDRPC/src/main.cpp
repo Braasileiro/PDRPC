00529e13967d659f6a53a9abdda0a1d2b0da6ccc
Lucas Cota <lucasbrunocota@live.com>
2022-07-23 04:17:31 -0300

    New SigScans. Needs MORE testing.

--- PDRPC/src/main.cpp
+++ PDRPC/src/main.cpp
@@ -1,14 +1,13 @@
 #include "pch.h"
 
 // Mod Library
-int m_ProcessId;
 HMODULE m_Library;
 
 // Mod Types
 typedef void(__cdecl* _OnInit)(int processId);
 typedef void(__cdecl* _OnDispose)();
 
-// Mod Pointers
+// Mod Functions
 _OnInit p_OnInit;
 _OnDispose p_OnDispose;
 
@@ -27,21 +26,13 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 	case DLL_THREAD_DETACH:
 		break;
 	case DLL_PROCESS_DETACH:
-		if (m_Library)
-		{
-			// Mod Dispose
-			p_OnDispose();
-		}
+		if (m_Library) p_OnDispose();
 		break;
 	}
 
 	return TRUE;
 }
 
-
-/*
- * Core Library
- */
 bool LoadModLibrary()
 {
 	m_Library = LoadLibraryA("PDRPC.Core.dll");
@@ -51,13 +42,43 @@ bool LoadModLibrary()
 		p_OnInit = (_OnInit)GetProcAddress(m_Library, "OnInit");
 		p_OnDispose = (_OnDispose)GetProcAddress(m_Library, "OnDispose");
 
-		// Current Process
-		m_ProcessId = GetCurrentProcessId();
-
 		return true;
 	}
 
 	return false;
+}
+
+
+/*
+ * Hooks
+ */
+
+// 1.02: 0x14043B2D0
+SIG_SCAN
+(
+	sigSongStart,
+	0x14043B2D0,
+	"\x8B\xD1\xE9\xA9\xE8\xFF\xFF\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xCC\xE9",
+	"xxxxxxx?????????x"
+);
+
+// 1.02: 0x14043B000
+SIG_SCAN
+(
+	sigSongEnd,
+	0x14043B000,
+	"\x48\x89\x5C\x24\x08\x57\x48\x83\xEC\x20\x48\x8D\x0D\xCC\xCC\xCC\xCC\xE8\xCC\xCC\xCC\xCC\x48\x8B\x3D\xCC\xCC\xCC\xCC\x48\x8B\x1F\x48\x3B\xDF",
+	"xxxxxxxxxxxxx????x????xxx????xxxxxx"
+);
+
+HOOK(void, __fastcall, _SongStart, sigSongStart(), int songId)
+{
+	original_SongStart(songId);
+}
+
+HOOK(__int64, __cdecl, _SongEnd, sigSongEnd())
+{
+	original_SongEnd();
 }
 
 
@@ -69,7 +90,11 @@ extern "C" __declspec(dllexport) void Init()
 	// Load Mod Library
 	if (LoadModLibrary())
 	{
+		// Install Hooks
+		INSTALL_HOOK(_SongStart);
+		INSTALL_HOOK(_SongEnd);
+
 		// Mod Entry Point
-		p_OnInit(m_ProcessId);
+		p_OnInit(GetCurrentProcessId());
 	}
 }
