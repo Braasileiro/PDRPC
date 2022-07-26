bc747a9061fc72083f72f22819f8aa34a5f7d7ba
Lucas Cota <lucasbrunocota@live.com>
2022-07-25 22:04:37 -0300

    Unique solution for the two projects.

--- build.bat
+++ build.bat
@@ -1,8 +1,4 @@
 @echo off
 
-REM Native
-call msbuild "PDRPC\PDRPC.sln" -t:Rebuild -p:Configuration=Release -p:Platform=x64
-
-REM Managed
-call msbuild "PDRPC.Core\PDRPC.Core.sln" -t:restore
-call msbuild "PDRPC.Core\PDRPC.Core.sln" -t:Rebuild -p:Configuration=Release -p:Platform="Any CPU"
+call msbuild "PDRPC.sln" -t:Restore -p:Configuration=Release
+call msbuild "PDRPC.sln" -t:Rebuild -p:Configuration=Release -p:Platform=x64
