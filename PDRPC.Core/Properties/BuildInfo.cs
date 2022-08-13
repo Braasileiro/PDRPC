9efd7bd398c248a3745641a7a948c93f764f27c6
Lucas Cota <lucasbrunocota@live.com>
2022-08-13 17:15:08 -0300

    Updating build info.

--- PDRPC.Core/Properties/BuildInfo.cs
+++ PDRPC.Core/Properties/BuildInfo.cs
@@ -5,7 +5,7 @@
         public const string Name = "PDRPC";
         public const string Description = "Discord Rich Presence for Hatsune Miku: Project DIVA Mega Mix+.";
         public const string Author = "Brasileiro";
-        public const string Company = "I love MS Lyrical Nanoha.";
+        public const string Company = "I love Mahou Shoujo Lyrical Nanoha.";
         public const string Version = "0.1.5";
     }
 }
