b51c9041646e0a36b794d8edddb1cdcf2c0336a1
Lucas Cota <lucasbrunocota@live.com>
2022-08-19 01:16:36 -0300

    Inverted values.

--- PDRPC.Core/Properties/Settings.cs
+++ PDRPC.Core/Properties/Settings.cs
@@ -11,6 +11,6 @@
     public const ulong SongMusicAddress = 0x0CC0B618;
     public const ulong SongDifficultyAddress = 0x016E2B90;
     public const ulong SongDifficultyExtraAddress = 0x016E2B94;
-    public const ulong SongPvFlagAddress = 0x016E2BA8;
-    public const ulong SongPracticeFlagAddress = 0x016E2BA9;
+    public const ulong SongPracticeFlagAddress = 0x016E2BA8;
+    public const ulong SongPvFlagAddress = 0x016E2BA9;
 }
